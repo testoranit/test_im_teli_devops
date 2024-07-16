2958e9e7a0847a46744bc7570d68e43cc18b15ce
Nitish Shishupal <testoranit@gmail.com>
2024-07-16 16:44:46 +0530

    final commit

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -27,9 +27,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "gitopsterrastate"
+    bucket = "mysthlmbuck16072024"
     key    = "terraform.tfstate"
-    region = "us-east-2"
+    region = "eu-north-1"
   }
 
   required_version = "~> 1.6.3"
